1702e3e92a7e1228939a877fa7eab3bb4e2f0735
Drea Cologgi <Andrea_Cologgi@hms.harvard.edu>
2023-05-12 14:01:47 -0400

    Add script randomizing action button destination
    Script will send users to random recipes on click.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Pancake menu script
 function openNav() {
     document.getElementById("myNav").style.height = "100%";
   }
@@ -5,3 +6,11 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.height = "0%";
   }
+
+//Homepage action button script
+const linkList = ['/recipes/skause.html','/recipes/flatbread.html','/recipes/elderberry.html'];
+
+document.getElementById("action").onclick = function() {
+  let randomNumber = Math.floor(Math.random()*3);
+  window.open(linkList[randomNumber],"_self")
+};
